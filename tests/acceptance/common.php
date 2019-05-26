122ccc9d9ebf19049199b476418a7f6b828791af
Sirin k <sirin.kolasseri@pantacor.com>
2019-05-26 08:02:14 +0530

    Bug fixes

---
+++ tests/acceptance/common.php
@@ -0,0 +1,15 @@
+<?php
+define('CP_BASIC','//*[@id="user-register-form"]/div/div[1]/div[2]/div[1]/div/a');
+define('CP_BASIC_UPGRADE','/html/body/section[2]/div/div[2]/div[1]/div[2]/div[1]/div/a');
+                           
+
+define('CP_PLUS','//*[@id="user-register-form"]/div/div[1]/div[2]/div[2]/div/a');
+define('CP_PLUS_UPGRADE','/html/body/section[2]/div/div[2]/div[1]/div[2]/div[2]/div/a');
+
+define('CP_FAMILY','//*[@id="user-register-form"]/div/div[1]/div[2]/div[3]/div/a');
+define('CP_FAMILY_UPGRADE','/html/body/section[2]/div/div[2]/div[1]/div[2]/div[3]/div/a');
+
+define('ADULT_TICKET','//*[@id="list_tickets"]/div[1]/article/div/button[2]');
+define('MEMBERSHIP_PURCHASED_SUCCESSFULLY','MEMBERSHIP PURCHASED SUCCESSFULY');
+define('TICKET_PURCHASED_SUCCESSFULLY','TICKET PURCHASED SUCCESFULLY');
+?>
